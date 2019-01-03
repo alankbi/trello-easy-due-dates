178da8ab4798177c7ed54c3e9337eac652f4804c
Alan-Bi <alan.bi326@gmail.com>
2019-01-02 23:54:18 -0800

    Add code to call Trello REST API

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
+function formatDate(date) {
+    var year = date.getFullYear();
+    var month = (date.getMonth() + 1).toString();
+    month = month.length > 1 ? month : '0' + month;
+    var day = date.getDate().toString();
+    day = day.length > 1 ? day : '0' + day;
+    
+    return year + '-' + month + '-' + day;
+}
+
 function onShiftForward(t) {
-    return t.card('due').then(function (value) {
-        var newDate = new Date(value.setDate(value.getDate() - 1));
+    return t.card('id', 'due').then(function (value) {
+        var newDate = new Date(value.due.setDate(value.due.getDate() - 1));
+        setNewDate(value.id, newDate);
         return Promise.resolve(newDate);
     });
 }
 
 function onShiftBack(t) {
 
+}
+
+function setNewDate(id, date) {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener('readystatechange', function () {
+      if (this.readyState === this.DONE) {
+        console.log(this.responseText);
+      }
+    });
+    
+    xhr.open('PUT', 'https://api.trello.com/1/cards/' + id + '/due/?value=' + formatDate(date));
+    xhr.send();
 }
 
 TrelloPowerUp.initialize({
--- script.test.js
+++ script.test.js
@@ -1,17 +1,23 @@
 var mockCardObject = {
-    card: function (str) {
-        if (str === 'due') {
-            return Promise.resolve(new Date('1/2/19'));
-        } else {
-            return Promise.resolve();
-        }
+    card: function (str1, str2) {
+        var cardObject = {};
+        cardObject[str1] = 1;
+        cardObject[str2] = new Date('1/2/19');
+        return Promise.resolve(cardObject);
     },
 }
 
+function testFormatDate() {
+    console.log('Test formatDate: should format 1/12/19 to be 2019-01-12')
+    var date = new Date('1/12/19');
+    var result = formatDate(date);
+    console.log('Expected: 2019-01-12\tActual: ' + result);
+}
+
 function testOnShiftForward() {
-    console.log('Test onShiftFoward: should shift 1/2/19 forward one day to 1/1/19')
     var result = onShiftForward(mockCardObject);
     result.then(function (value) {
+        console.log('Test onShiftFoward: should shift 1/2/19 forward one day to 1/1/19')
         console.log('Expected: 1/1/19\tActual: ' + value.toLocaleDateString('en-US'));
     });
 }
@@ -23,6 +29,7 @@ function testOnShiftBack() {
 
 function runAllTests() {
     console.log('All Tests:');
+    testFormatDate();
     testOnShiftForward();
     testOnShiftBack();
 }
