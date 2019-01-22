36fcc688156ef150fc77a377fdba9b55b3fe1864
Alan-Bi <alan.bi326@gmail.com>
2019-01-21 17:01:22 -0800

    Clean up files

--- js/parse.js
+++
@@ -1,9 +0,0 @@
-var t = window.TrelloPowerUp.iframe({
-    appKey: '947bb1a338f0b8679fb6de16422d1b20',
-    appName: 'Easy Due Dates'
-});
-
-function parseJSON(json) {
-    console.log('here!');
-    console.log(JSON.stringify(json));
-}
--- webhook.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var server = http.createServer(function (request, response) {
-    if (request.method == 'POST') {
-        
-        response.end('received POST request.');
-    }
-});
-
-server.listen(8000);
